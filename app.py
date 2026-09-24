5bf6f604d99a073767948435fba0dcf2c9c27316
HeidyCoello <hcoello95@icloud.com>
2026-09-23 21:05:55 -0500

    Add file uploader for CSV file input

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Heidy Coello")
 
+
 archivo = st.file_uploader("Cargue su archivo")
+
+if archivo is not None:
 tabla = pd.read_csv(archivo)
 st.write(tabla)
 
