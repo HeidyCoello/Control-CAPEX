ceabc70b3b0103b46a2fa1d8900aa6372a3a0ba0
HeidyCoello <hcoello95@icloud.com>
2026-09-23 21:02:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Heidy Coello")
+
+archivo = st.file_uploader("Cargue su archivo")
