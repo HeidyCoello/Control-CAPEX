ec8e2d34e4dac8992b48a1c1a92e05095f7fd33e
HeidyCoello <hcoello95@icloud.com>
2026-09-23 21:05:01 -0500

    Add file uploader and display CSV data

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parametros")
 st.write("Elaborado por: Heidy Coello")
 
 archivo = st.file_uploader("Cargue su archivo")
+tabla = pd.read_csv(archivo)
+st.write(tabla)
+
